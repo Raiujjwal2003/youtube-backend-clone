10d9cd0dfce49d35ca6c3723041638907fc48dff
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-01-08 22:17:04 +0530

    utils file configuration

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
-}))
+})) 
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+// const asyncHandler = (fn) = async()=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
+
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
