d0d8d87743cf47035336c5a594c38f527684d4c2
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-01-24 23:43:03 +0530

    add controller and routes for user

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//  routes import
+ 
+import userRouter from './routes/user.routes.js'
+
+// router decleration
+
+app.use("/api/v1/users", userRouter)
 
 
 export {app}
