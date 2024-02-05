427b9e5ae9c1827905e8e196c48427d224a160d7
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-02-05 14:00:59 +0530

    add all controller  and routes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000 ,() =>{
-        console.log(`Server is running on port:  ${process.env.PORT}`)
+        console.log(`⚙️ Server is running on port:  ${process.env.PORT}`)
     })
 })
 .catch((error) =>{
